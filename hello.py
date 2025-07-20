e71ac0e5876ea0a8a4ffdcbc41af3694679f7bf0
wloyd <info@twconsulting.biz>
2025-07-20 20:27:07 +0100

    1 of manyw

--- hello.py
+++ hello.py
@@ -1,1 +1,7 @@
-print("Hello world")
+# print("Hello world")
+fnum = input("What is the first number ")
+snum = input("What is the second number ")
+if fnum > snum:
+    print("The first number is greater than the second number")
+else:
+    print("The second number is greater than the first number")
